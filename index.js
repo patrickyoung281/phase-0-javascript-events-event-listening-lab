a6abb7ad968d13dc8e884ae3b4f2ef39c1f38471
Patrick Young <patrickyoung281@gmail.com>
2022-05-23 19:26:00 -0400

    commit tree 07705c9b36f1f395a61dac0dfc2182d60c05e66b

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
+
+function addingEventListener () {
+
+const input = document.getElementById("input");
+
+input.addEventListener("click", function () {
+  alert("I was clicked!");
+})};
+
+
+
+
+
+
+
+
+
+
+/*
+
 function addingEventListener() {
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+*/
+
